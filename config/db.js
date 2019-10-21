8921d54c423de216ab73ce6c3d51e3ac3dbe9d95
misheal <ezekielmisheal4@gmail.com>
2019-10-21 14:10:49 +0100

    updated

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 module.exports = {
     // ==========database connection locally=========================
-    // MONGOURL: 'mongodb://localhost/appointmentee'
+    MONGOURL: 'mongodb://localhost/appointmentee'
 
 
     // =============== database connection to mongodb atlas============
-    MONGOURL:'mongodb://misheal:misheal@cluster0-shard-00-00-zxaqu.mongodb.net:27017,cluster0-shard-00-01-zxaqu.mongodb.net:27017,cluster0-shard-00-02-zxaqu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+    // MONGOURL:'mongodb://misheal:misheal@cluster0-shard-00-00-zxaqu.mongodb.net:27017,cluster0-shard-00-01-zxaqu.mongodb.net:27017,cluster0-shard-00-02-zxaqu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 }
